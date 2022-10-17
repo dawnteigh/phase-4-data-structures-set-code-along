27ed9b381dd00e0a785a9c53341c80bac2b6b0c8
Donte Ladatto <donte279@gmail.com>
2022-10-17 13:52:35 -0500

    commit tree 91ad0a281eb4aa67bec01ac3a54010ee02058586

--- lib/my_set.rb
+++ lib/my_set.rb
@@ -1,1 +1,28 @@
-# your code here
+class MySet
+
+  def initialize(enumerable = [])
+    @hash = {}
+    enumerable.each do |v|
+      @hash[v] = true
+    end
+  end
+
+  def include?(v)
+    @hash.has_key?(v)
+  end
+
+  def add(v)
+    @hash[v] = true
+    self
+  end
+
+  def delete(v)
+    @hash.delete(v)
+    self
+  end
+
+  def size
+    @hash.size
+  end
+
+end
